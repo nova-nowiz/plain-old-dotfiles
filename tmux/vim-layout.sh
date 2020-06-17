ca4c49861fa0227cf17f70ea974551b0c5aceaf1
Narice <vincent.mouillon@gmail.com>
2020-06-17 21:59:01 +0200

    moving to git bare organization

--- tmux/vim-layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-tmux kill-session -a
-tmux new-session -c ~/Documents/code/Game/src \; \
-  send-keys 'vim' C-m \; \
-  split-window -h -p 35 -c ~/Documents/code/Game/build \; \
-  select-pane -t 0\;
