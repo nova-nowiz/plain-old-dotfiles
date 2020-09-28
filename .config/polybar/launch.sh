41e790085756aae34d1b82688f87e60438877e9d
Narice <narice@tutanota.com>
2020-09-28 10:20:45 +0200

    fixed polybar lauch script for nixos and changed secondary color

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+ps -ef | grep polybar\ mybar$ | awk '{print $2}' | xargs kill -9
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
